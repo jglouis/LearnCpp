9c0828ec3a8641486863ccaf3f898b7815bbccde
Jean-Guillaume Louis <jguillame.louis@gmail.com>
2016-03-22 20:26:05 +0100

    Declare area method const

--- LearnCpp/shape.h
+++ LearnCpp/shape.h
@@ -2,19 +2,19 @@
 
 class Shape {
 public:
-	virtual int area() = 0;
+	virtual const int area() = 0;
 };
 
 class Rectangle : public Shape {
 	int width, height;
 public:
 	Rectangle(int w, int h) : width(w), height(h) {};
-	int area() { return width * height; };
+	const int area() { return width * height; };
 };
 
 class Triangle : public Shape {
 	int base, height;
 public:
 	Triangle(int b, int h) : base(b), height(h) {};
-	int area() { return base * height / 2; };
+	const int area() { return base * height / 2; };
 };
