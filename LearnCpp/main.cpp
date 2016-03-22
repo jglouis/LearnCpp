0948babb1f9bf643a4c4814c0b9df7c34674ae24
Jean-Guillaume Louis <jguillame.louis@gmail.com>
2016-03-22 20:23:51 +0100

    Use a Vector of abstract Shape

--- LearnCpp/main.cpp
+++ LearnCpp/main.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
+#include <vector>
 #include "shape.h"
 
 int main() {
 	Rectangle rect(3, 4);
 	Triangle triang(3, 4);
 
-	std::cout << "rect area: " << rect.area() << std::endl;
-	std::cout << "triang area: " << triang.area() << std::endl;
+	std::vector<Shape*> shapes(2);
+	shapes[0] = &rect;
+	shapes[1] = &triang;
+
+
+	std::cout << "rect area: " << shapes[0]->area() << std::endl;
+	std::cout << "triang area: " << shapes[1]->area() << std::endl;
 
 	return 0;
 }
